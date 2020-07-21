1ec151da367015d40fe0dc5ca162a0834a73e880
Greek <ygreek@gmail.com>
2020-07-21 18:31:16 +0800

    Replicated codes to fit six modules

---
+++ src/places/components/Customisation/submitted/SubmittedFive.js
@@ -0,0 +1,37 @@
+import PushArray from "../PushArray";
+
+const SubmittedFive = async (
+  modArr1,
+  modArr2,
+  modArr3,
+  modArr4,
+  modArr5,
+  items,
+  history
+) => {
+  PushArray(modArr1, items.module1, items.information1.classes);
+  PushArray(modArr2, items.module2, items.information2.classes);
+  PushArray(modArr3, items.module3, items.information3.classes);
+  PushArray(modArr4, items.module4, items.information4.classes);
+  PushArray(modArr5, items.module5, items.information5.classes);
+
+  try {
+    const req = await fetch("http://localhost:5000/api/create/fiveCustomised", {
+      method: "POST",
+      body: JSON.stringify({
+        mod1Array: modArr1,
+        mod2Array: modArr2,
+        mod3Array: modArr3,
+        mod4Array: modArr4,
+        mod5Array: modArr5,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const res = await req.json();
+    console.log(res);
+    history.push(`/create/timetableInformation/${res.customModule._id}`);
+    //redirect to new page
+  } catch (err) {}
+};
+
+export default SubmittedFive;
